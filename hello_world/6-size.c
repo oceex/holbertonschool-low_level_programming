385e0769e0a0e12bd1350934596d72370d85db8c
leen <leenalgraawi@gmail.com>
2026-02-22 17:32:51 +0000

    sizeiii

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
- * main 
- * Description: this program should prints the sizes of the this types 
+ * main
+ * Description: this program should prints the sizes of the this types
  * Return: 0
  */
 int main(void)
 {
-printf("%s%d %s\n%s%d %s\n%s%d %s\n%s%d %s\n%s%d %s\n",
-"Size of a char: ", sizeof(char), "byte(s)",
-"Size of an int: ", sizeof(int), "byte(s)",
-"Size of a long int: ", sizeof(long int), "byte(s)",
-"Size of a long long int: ", sizeof(long long int), "byte(s)",
-"Size of a float: ", sizeof(float), "byte(s)",
+printf("%s%d %s\n%s%d %s\n%s%d %s\n%s%d %s\n%s%d %s\n"
+, "Size of a char: ", sizeof(char), "byte(s)"
+, "Size of an int: ", sizeof(int), "byte(s)"
+, "Size of a long int: ", sizeof(long int), "byte(s)"
+, "Size of a long long int: ", sizeof(long long int), "byte(s)"
+, "Size of a float: ", sizeof(float), "byte(s)"
 );
 return (0);
 }
