5accd201e48d555a95ad06e0275943f9dc19b6da
leen <leenalgraawi@gmail.com>
2026-02-22 23:43:30 +0000

    numchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+/**
+  * main - this is the starting main and its output the numbers as a char ;>
+  * Description: this program should prints all the numbers with one digit
+  * Return: 0
+  */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
