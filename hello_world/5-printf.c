c63f691926dc8d997c768d7aaff0e0832fc9c3df
leen <leenalgraawi@gmail.com>
2026-02-22 16:52:57 +0000

    haha

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Prints with proper grammar, but the outcome is a piece of art, /with a new line
+ * main - Prints with proper grammar, but the outcome is a piece of art,
  * Description: this is my first c program!
  * Return: 0
  */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
