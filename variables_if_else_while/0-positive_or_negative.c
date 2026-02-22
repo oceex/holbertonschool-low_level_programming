cb35e070243401c5d6db608df3d5ba7996594c0b
leen <leenalgraawi@gmail.com>
2026-02-22 21:53:32 +0000

    first 'if' ;>

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**  main - this program should deside whether the random number
+/**
+  *  main - this program should deside whether the random number
   *  is pos or nig or zero.
   * Descption: prints negative if it - and positive\n if it + and zero if it 0
   * AND
