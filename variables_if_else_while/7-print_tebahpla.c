5d17f32e9fa731d5c73465e823cbaa0bdc02329b
leen <leenalgraawi@gmail.com>
2026-02-22 23:48:24 +0000

    revers alpha

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+/**
+  * main - this is the starting main and its output the alphabet in revers
+  * Description: this program should prints all the alphabet on lowercase
+  * Return: 0
+  */
+int main(void)
+{
+int i;
+
+for (i = 122; i>=97; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
