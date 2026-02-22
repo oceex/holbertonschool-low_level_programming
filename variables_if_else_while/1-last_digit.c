db19f7925cab12944aea87db5c5576ead239f78c
leen <leenalgraawi@gmail.com>
2026-02-22 22:18:03 +0000

    using moood

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,11 +17,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 m = n % 10;
 printf("Last digit of %d is %d and is ", n, m);
-if(m == 0)
+if (m == 0)
 {
 printf("%d\n", m);
 }
-else if(m > 5)
+else if (m > 5)
 {
 printf("greater than 5\n");
 }
