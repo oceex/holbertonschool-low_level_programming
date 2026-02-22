2e86c0f9dff849a886c2c6bafb010e4aa95740bc
leen <leenalgraawi@gmail.com>
2026-02-22 23:34:49 +0000

    nummmberss

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+/**
+  * main - this is the starting main and its output the numbers
+  * Description: this program should prints all the numbers with one digit
+  * Return: 0
+  */
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+printf(i);
+}
+printf("\n");
+return (0);
+}
