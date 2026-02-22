bf4822723631ff3f5608f3ca1f69bfa4edc0c42b
leen <leenalgraawi@gmail.com>
2026-02-22 17:29:16 +0000

    sizeiii

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 printf("%s%d %s\n%s%d %s\n%s%d %s\n%s%d %s\n%s%d %s\n"
-,"Size of a char: ",sizeof(char),"byte(s)"
-,"Size of an int: ",sizeof(int),"byte(s)"
-,"Size of a long int: ",sizeof(long int),"byte(s)"
-,"Size of a long long int: ",sizeof(long long int),"byte(s)"
-,"Size of a float: ",sizeof(float),"byte(s)"
+, "Size of a char: ", sizeof(char), "byte(s)"
+, "Size of an int: ", sizeof(int), "byte(s)"
+, "Size of a long int: ", sizeof(long int), "byte(s)"
+, "Size of a long long int: ", sizeof(long long int), "byte(s)"
+, "Size of a float: ", sizeof(float), "byte(s)"
 );
 return (0);
 }
