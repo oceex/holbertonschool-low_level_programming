ffa6a67ae55c04ae864ae41170c4e284796bb807
leen <leenalgraawi@gmail.com>
2026-02-22 23:14:49 +0000

    ALPHA

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+/**
+  * main - this is the starting main and its output the alphabet
+  * Description: this program should prints all the alphabet
+  * on lowercase AND uppercase
+  * Return: 0
+  */
+int main(void)
+{
+int i;
+for ( i = 97; i <= 122; i++)
+{
+putchar(i);
+}
+for ( i = 65; i <= 90; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
