5f9dc6ca6855852c2bb5a981c70e355ad9d1100f
leen <leenalgraawi@gmail.com>
2026-02-22 15:33:46 +0000

    k?

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * my_firts_function - this program should prints a thing ;>
+ * my_firts_main - this program should prints a thing ;>
+ * main should return 0
  */
 int main(void)
 {
