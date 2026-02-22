41b6cd989a3826ee55de75f3eb713c002e58ffd2
leen <leenalgraawi@gmail.com>
2026-02-22 21:42:44 +0000

    first if ;>

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,18 +12,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf(n);
 	if (n>0)
 	{
-	printf(" is negative\n");
+	printf("%d is negative\n",n);
 	}
         else if (n>0)
         {
-        printf(" is positive\n");
+        printf("%d is positive\n",n);
         }
         else
         {
-        printf(" is zero\n");
+        printf("%d is zero\n",n);
         }
 	return (0);
 }
