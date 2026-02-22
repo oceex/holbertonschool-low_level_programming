961b63b544f4deeb1ad68aa937407e82e6ece551
leen <leenalgraawi@gmail.com>
2026-02-22 22:14:19 +0000

    using moood

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  * main - this program should know what the LAST DIGIT
+  * in the random nomber geven
+  * Description: this program should prints the last digit
+  * and desid whither if it more than 5 or not
+  * Return: 0
+  */
+int main(void)
+{
+int n;
+int m = n%10;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("Last digit of %d is %d and is ", n, m);
+if(m == 0)
+{
+printf("%d\n", m);
+}
+else if(m > 5)
+{
+printf("greater than 5\n",);
+}
+else
+{
+printf("less than 6 and not 0\n",);
+}
+return (0);
+}
