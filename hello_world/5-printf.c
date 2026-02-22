4e8238d60cf6be2b3bfadd6cf72fc41ec9fdd9aa
leen <leenalgraawi@gmail.com>
2026-02-22 16:46:46 +0000

    haha

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints "Programming is like building a multilingual puzzle
+ * Description: this is my first c program!
+ * Return: 0
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
