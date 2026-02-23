2c70b5b4246d9d7dd042e0436bf0a15fd5e6bb13
leen <leenalgraawi@gmail.com>
2026-02-23 17:59:11 +0000

    hexihexi

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+/**
+  * main - this is the starting main and its output the hex numbers
+  * Description: this program should prints all the hex numbaers
+  * Return: 0
+  */
+int main(void)
+{
+int i;
+
+for (i = 48; i <= 102; i++)
+{
+if (i >= 48 && i<= 57 || i >= 97 && i<= 102)
+{
+putchar(i);
+}
+}
+putchar('\n');
+return (0);
+}
