f350fd1a7a6d636261e75dfb41b6e726be4e63fc
leen <leenalgraawi@gmail.com>
2026-02-23 19:01:36 +0000

    real realll combb

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+# include <stdio.h>
+/**
+  * main - this is the starting main and its output all three digits number
+  * Description: this program should prints all the numbers in 3 digits
+  * Return: 0
+  */
+int main(void)
+{
+int i;
+int j;
+int k;
+
+for (i = 0; i <= 9; i++)
+{
+for (j = i + 1; j <= 9; j++)
+{
+for (k = j + 1; k <= 9; k++)
+{
+putchar(i + 48);
+putchar(j + 48);
+putchar(k + 48);
+if (!((k == 9) && (j == (k - 1)) && (i == (j - 1))))
+}
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
+
+
