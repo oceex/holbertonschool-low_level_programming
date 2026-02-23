8a3f22a1b1a47d1daffbebcbababc1e67c6e477d
leen <leenalgraawi@gmail.com>
2026-02-23 18:48:52 +0000

    reall commb

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,12 @@ int main(void)
 int i;
 int j;
 
-for(i = 0; i <= 8; i++)
+for (i = 0; i <= 8; i++)
 {
-for(j = i + 1; j <= 9; j++)
+for (j = i + 1; j <= 9; j++)
 {
-putchar(i+48);
-putchar(j+48);
+putchar(i + 48);
+putchar(j + 48);
 if (!((i == 8) && (j == 9)))
 {
 putchar(',');
