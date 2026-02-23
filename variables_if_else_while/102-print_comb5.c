082cd4e7fa83ea56fb20289d0ae2fca0af272cc9
leen <leenalgraawi@gmail.com>
2026-02-23 19:23:59 +0000

    4 digits >:(

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+# include <stdio.h>
+/**
+  * main - this is the starting main and its output 4 number digit o_0
+  * Description: this program should prints all 4 digit numbers
+  * Return: 0
+  */
+int main(void)
+{
+int i;
+int j;
+int m;
+int n;
+
+for (i = 0; i <= 9; i++)
+{
+for (j = i + 1; j <= 9; j++) //the big ones
+{
+for (m = i; m <= 9; m++)
+{
+for (n = j + 1; n <= 9; n++)
+{
+putchar(i + 48);
+putchar(j + 48);
+putchar(' ');
+putchar(m + 48);
+putchar(n + 48);
+if (!((n == 9) && (m == i) && (j == (i - 1) && (i == 9)))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
