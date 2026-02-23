68f81b0426b77a100d8ac03c3d82803785fea6f8
leen <leenalgraawi@gmail.com>
2026-02-23 20:09:17 +0000

    nah

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+  * main - this is the starting main and its output '_putchar'
+  * Description: this program should prints '_putchar'
+  * Return: 0
+  */
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
