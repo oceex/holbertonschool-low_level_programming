2078ffdfda3bbc4577e6f82ef88243d3f9e8b5ac
leen <leenalgraawi@gmail.com>
2026-02-24 14:46:12 +0000

    mainnig

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+# include "0-putchar.c"
+/**
+  * main - this is the starting main and its output the alphabet
+  * Description: this program should prints all the alphabet on lowercase
+  * Return: 0
+  */
+int main(void)
+{
+printing();
+return (0);
+}
