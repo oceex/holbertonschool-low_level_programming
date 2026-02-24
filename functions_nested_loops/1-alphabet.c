7370bbd1d32309832b059967c5d5728d736108aa
leen <leenalgraawi@gmail.com>
2026-02-24 18:30:14 +0000

    yaaarr

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+# include "main.h"
+/**
+  * print_alphabet - 
+  * Description: this program should prints all the alphabet on lowercase
+  * Return: 0
+  */
+print_alphabet()
+{
+int i;
+
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+
