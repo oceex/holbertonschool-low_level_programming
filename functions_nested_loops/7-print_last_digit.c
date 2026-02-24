1360ea754cb9820628dfdd69e46a0f18aa9d7cc0
leen <leenalgraawi@gmail.com>
2026-02-24 20:08:23 +0000

    lala

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+# include "main.h"
+/**
+  * print_last_digit - This function determines whether
+  * @x : the number
+  *
+  * Description: this program should prints
+  * Return: 0
+  */
+int print_last_digit(int x)
+{
+int k = x%10;
+_putchar(k+48);
+return (k);
+}
