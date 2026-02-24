dd8c6f32dbf105878e403f24bb401bd9540c4f43
leen <leenalgraawi@gmail.com>
2026-02-24 19:06:43 +0000

    haha

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+# include "main.h"
+/**
+  * _islower - this fun detirmaine whither if c is lower or not
+  * Description: this program should prints 1 if c is lower or 0 otherways
+  * Return: 0 or 1
+  */
+int _islower(int c)
+{
+return ((c >= 97)&&(c <= 122))
+}
