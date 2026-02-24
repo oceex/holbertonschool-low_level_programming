f5e9ed6f0cba349055c1bb71ffd62381da5752dc
leen <leenalgraawi@gmail.com>
2026-02-24 19:25:32 +0000

    letter

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+# include "main.h"
+/**
+  * _isalpha - This function determines whether if c is a letter or not
+  * @c : this is a int number
+  *
+  * Description: this program should print 1 for a letter and 0 otherways
+  * Return: 0 or 1
+  */
+int _isalpha(int c)
+{
+return ((c >= 97) && (c <= 122) || (c >= 65) && (c <= 90));
+}
+
