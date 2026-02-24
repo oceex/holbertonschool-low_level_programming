dcaffba13d09dca66a78daac0df22f77e3a816a3
leen <leenalgraawi@gmail.com>
2026-02-24 22:27:55 +0000

    adding

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+# include "main.h"
+/**
+  * add  - This function determines whether
+  * @a : para a
+  * @b : para b
+  *
+  * Description: this program should prints the sum of two numbers
+  * Return: a + b
+  */
+int add(int a, int b)
+{
+return (a + b);
+}
