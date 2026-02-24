b37737b18a68a3d27ef5aab290fa277b69eec421
leen <leenalgraawi@gmail.com>
2026-02-24 19:53:07 +0000

    yoyo

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+# include "main.h"
+# include <math.h>
+/**
+  *  - This function find the return the abslote value
+  *  @x
+  * Description: this program should return the abslote value
+  * Return: 0
+  */
+int _abs(int x)
+{
+return math.abs(x);
+}
