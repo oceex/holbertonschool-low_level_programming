473df36d4ce4aa1f94caad900c4836c1b40bd79b
leen <leenalgraawi@gmail.com>
2026-02-24 20:23:30 +0000

    houuurss

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,42 @@
+# include "main.h"
+/**
+  *  - This function determines whether
+  *
+  * Description: this program should prints
+  * Return: 0
+  */
+void jack_bauer(void)
+{
+int i;
+int j;
+int m;
+int n;
+int k;
+
+for (i = 0; i <= 2; i++)
+{
+if (i == 2)
+{
+k = 3;
+}
+else
+{
+k = 9;
+}
+for (j = 0; j <= k; j++)
+{
+for (m = 0; m <= 5; m++)
+{
+for (n = 0; n <= 9; n++)
+{
+_putchar(i+48);
+_putchar(j+48);
+_putchar(':');
+_putchar(m+48);
+_putchar(n+48);
+_putchar('\n');
+}
+}
+}
+}
+}
