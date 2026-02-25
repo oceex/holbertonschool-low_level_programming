9a63948dda7c00dad65eeebddf34640f23016297
leen <leenalgraawi@gmail.com>
2026-02-25 01:07:42 +0000

    yaar

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,12 +1,11 @@
 # include <stdio.h>
-# include "main.h"
 /**
-  * sumMulti_5_3  - This function sum all the numbers that containts 5 or 3
+  * main - This function sum all the numbers that containts 5 or 3
   *
   * Description: this program should prints the sum of all numbers that containts 3 or 5 bellow 1024
   * Return: void
   */
-void sumMulti_5_3(void)
+void main(void)
 {
 int i;
 int k = 0;
