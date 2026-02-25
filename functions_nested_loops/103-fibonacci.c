15f3f6850e4f34722246ed6e56a05d7171705b2c
leen <leenalgraawi@gmail.com>
2026-02-25 01:53:08 +0000

    fipunacccii

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+# include <stdio.h>
+/**
+  * main - This function will print the sum of all even numbers
+  * on fepunacci term less than 4m
+  * Description: this program should prints the sum
+  * Return: 0
+  */
+int main(void)
+{
+long  m = 1;
+long  n = 2;
+long  tmp = 0;
+long long sum = 0;
+while (m <= 4000000)
+{
+if (m % 2 == 0)
+{
+sum += m;
+}
+tmp = m + n;
+m = n;
+n = tmp;
+}
+printf("%lld\n", sum);
+return (0);
+}
