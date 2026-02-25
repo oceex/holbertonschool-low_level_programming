1300c13217fa3005ec14b59d74327f7f42cb5602
leen <leenalgraawi@gmail.com>
2026-02-25 02:07:02 +0000

    yaaarr

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+# include <stdio.h>
+/**
+  * main - This function prints the 98th fibonacci numbreesss
+  * Description: this program should prints fibonacciii
+  * Return: 0
+  */
+int main(void)
+{
+int i;
+long m = 1;
+long n = 2;
+long tmp;
+
+for (i = 0; i < 98; i++)
+{
+if (i != 97)
+{
+printf("%ld, ", m);
+}
+else
+{
+printf("%ld", m);
+}
+tmp = m + n;
+m = n;
+n = tmp;
+}
+printf("\n");
+return (0);
+}
