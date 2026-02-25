a2aea01652a842d6ae1846487d7d7b695cd3b204
leen <leenalgraawi@gmail.com>
2026-02-25 20:42:19 +0000

    h

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -9,17 +9,42 @@ int main(void)
 int i;
 unsigned long m = 1;
 unsigned long n = 2;
+int bon = 0;
 unsigned long tmp;
 
 for (i = 0; i < 98; i++)
 {
+if (i >= 90)
+{
+if (m > 4000000000)
+{
+bon++;
+m-=4000000000;
+}
 if (i != 97)
 {
+if (bon)
+{
+printf("4%ld, ", m);
+bon--;
+}
+else
+{
 printf("%ld, ", m);
+}
+}
+}
+else
+{
+if (bon)
+{
+printf("4%ld", m);
+bon--;
 }
 else
 {
 printf("%ld", m);
+}
 }
 tmp = m + n;
 m = n;
