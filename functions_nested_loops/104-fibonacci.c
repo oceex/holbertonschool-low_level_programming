aa7531a02ca943f7109abc25008612816673a54c
leen <leenalgraawi@gmail.com>
2026-02-26 19:17:57 +0000

    hmb

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,55 +1,43 @@
 # include <stdio.h>
 /**
   * main - This function prints the 98th fibonacci numbreesss
+  * @a1 : frist term
+  * @a2 : secound term
+  * @b1 : boolean value to hold the big digit
+  * @b2 : boolean value to hold the big digit
+  * @t1 : tmp to move the terms
+  * @t2 : anouther tmp
+  * @i  : to conun the terms
+  *
   * Description: this program should prints fibonacciii
   * Return: 0
   */
 int main(void)
 {
+unsigned long a1 = 1, a2 = 2;
+unsigned long b1 = 0, b2 = 0;
+unsigned long t1, t2;
 int i;
-unsigned long m = 1;
-unsigned long n = 2;
-int bon = 0;
-unsigned long tmp;
 
-for (i = 0; i < 98; i++)
+for (i = 1; i <= 98; i++)
 {
-if (i >= 90)
-{
-if (m > 4000000000)
-{
-bon++;
-m-=4000000000;
-}
-if (i != 97)
-{
-if (bon)
-{
-printf("4%ld, ", m);
-bon--;
-}
+if (b1)
+printf("%lu%010lu", b1, a1);
 else
-{
-printf("%ld, ", m);
+printf("%lu", a1);
+
+if (i != 98)
+printf(", ");
+
+t1 = a1 + a2;
+t2 = b1 + b2 + (t1 < a1);
+
+a1 = a2;
+a2 = t1;
+b1 = b2;
+b2 = t2;
 }
-}
-}
-else
-{
-if (bon)
-{
-printf("4%ld", m);
-bon--;
-}
-else
-{
-printf("%ld", m);
-}
-}
-tmp = m + n;
-m = n;
-n = tmp;
-}
+
 printf("\n");
 return (0);
 }
