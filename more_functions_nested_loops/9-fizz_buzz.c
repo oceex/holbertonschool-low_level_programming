59066a62aae19aa76c3eaf0e430df7ce3bc0da54
leen <leenalgraawi@gmail.com>
2026-02-28 23:25:02 +0000

    ll

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -16,7 +16,7 @@ if ((i % 3 == 0) || (i % 5 == 0))
 if (i % 3 == 0)
 printf(" Fizz");
 if (i % 5 == 0)
-i % 3 == 0 ? printf("Buss") : printf(" Buzz");
+i % 3 == 0 ? printf("Buzz") : printf(" Buzz");
 }
 else
 printf(" %d", i);
