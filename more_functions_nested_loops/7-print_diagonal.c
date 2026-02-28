af18f6bd82b8e495dea2783c9e49072bbe1d478d
leen <leenalgraawi@gmail.com>
2026-02-28 22:52:55 +0000

    haha

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,11 +1,15 @@
 # include "main.h"
 /**
-  * print_diagonal - this method will print a digonal line with \
-  * @ n : an int number will be the times of \
-  *
-  * Description: this will print \ as a digonal line with n times
-  * Return: void
-  */
+ * print_diagonal - Prints a diagonal line using the '\' character.
+ * @n: Number of times the character '\' should be printed.
+ *
+ * Description: Each '\' is printed on a new line with increasing
+ *              indentation to form a diagonal. If n is 0 or less,
+ *              only a newline is printed.
+ *
+ * Return: void
+ */
+
 void print_diagonal(int n)
 {
 int i;
