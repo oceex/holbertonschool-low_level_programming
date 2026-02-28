a4b4355cc0215b68d554fb5ca2c33cb183d78d0b
leen <leenalgraawi@gmail.com>
2026-02-28 22:33:55 +0000

    digonaal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+# include "main.h"
+/**
+  * print_diagonal - this method will print a digonal line with \
+  * @n : n will be the times of \
+  * Description: this will print \ as a digonal line with n times
+  * Return: void
+  */
+void print_diagonal(int n)
+{
+int i;
+int j;
+for (i = 0; i < n ; i++)
+{
+for (j = 0; j < n ; j++)
+{
+if ( j == i)
+_putchar('\\');
+else
+_putchar(' ');
+}
+_putchar('\n');
+}
+}
