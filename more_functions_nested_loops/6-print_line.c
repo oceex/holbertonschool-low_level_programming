1b5cb6e59a402d01b8066b43a6ccfd0e992a8eb1
leen <leenalgraawi@gmail.com>
2026-02-28 22:23:18 +0000

    lineliiine

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+# include "main.h"
+/**
+  * print_line - this method will print a stight line
+  * @n : is the line length
+  *
+  * Description: this will print acording to n the line length
+  * Return: void
+  */
+void print_line(int n)
+{
+for (; n > 0; n--)
+_putchar('_');
+_putchar('\n');
+}
