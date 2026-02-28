580d5cc1ba55b475bd9466ad73639bf7393f333e
leen <leenalgraawi@gmail.com>
2026-02-28 21:29:56 +0000

    multiii

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+# include "main.h"
+/**
+  * _isdigit - this method will return a * b
+  * @a : first int
+  * @b : second int
+  *
+  * Description: this will return a * b
+  * Return: a * b
+  */
+int mul(int a, int b)
+{
+return (a * b);
+}
