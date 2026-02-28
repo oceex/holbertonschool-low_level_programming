b9c4f6aa29f87cd2277b468b31e6715f638ba64d
leen <leenalgraawi@gmail.com>
2026-02-28 22:13:20 +0000

    tricky 14 ;>

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+# include "main.h"
+/**
+  * more_numbers  - this method will print from 0-14 10 times
+  * Description: this will print 01234567891011121314 *10
+  * Return: void
+  */
+void more_numbers(void)
+{
+int i;
+int j;
+int x;
+
+for (j = 0; j <= 9; j++)
+{
+for (i = 0; i <= 14; i++)
+{
+x = i > 9 ? 10 - i : i ;
+if (i >= 10)
+_putchar('1');
+_putchar(x + 48);
+}
+_putchar('\n');
+}
+}
