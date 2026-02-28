e1fc3ae8966c389dafc674d67fb85e3c377561c0
leen <leenalgraawi@gmail.com>
2026-02-28 23:17:05 +0000

    ll

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+/**
+ * main - Prints Pizz for the multi of 3 and a Buss for a multi of 5 or the number it self.
+ * Description: print from 1 to 100
+ *
+ * Return: void
+ */
+int main(void)
+{
+int i;
+printf("1");
+for (i = 2; i <= 100; i++)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+{
+if (i % 3 == 0)
+printf(" Fizz");
+if (i % 5 == 0)
+printf(" Buzz");
+}
+else
+printf(" %d", i);
+}
+return (0);
+}
