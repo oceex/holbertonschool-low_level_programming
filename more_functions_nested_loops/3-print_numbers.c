6b9766dcce5564ee5925dfe1548b0451931a3f4e
leen <leenalgraawi@gmail.com>
2026-02-28 21:37:01 +0000

    digitinng

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+# include "main.h"
+/**
+  * print_numbers - this method will print from 0-9
+  * Description: this will print 0123456789
+  * Return: void
+  */
+void print_numbers(void)
+{
+int i;
+
+for(i = 48; i <= 57; i++)
+_putchar(i);
+_putchar('\n');
+}
+
+
+
+
