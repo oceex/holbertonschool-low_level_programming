3ca0e2ca309461af9016679e6496518714f617cd
leen <leenalgraawi@gmail.com>
2026-02-28 23:00:48 +0000

    squsqu

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+# include "main.h"
+/**
+ * print_square - Prints a square  using the '#' character.
+ * @size: The size of the square.
+ *
+ * Description: The size of the square will be size * size
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+int i;
+int j;
+
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
