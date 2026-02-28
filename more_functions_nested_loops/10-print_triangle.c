22696e3c6c58b299fd7580fe92937a6f04bb9ba4
leen <leenalgraawi@gmail.com>
2026-02-28 23:45:20 +0000

    tritri

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+# include "main.h"
+/**
+ * print_triangle - prints a right-aligned triangle using the '#' character
+ * @size: the height of the triangle
+ *
+ * Description: If size is greater than 0, the function prints a triangle
+ * where each line contains spaces followed by '#' characters, forming a
+ * right-aligned shape. If size is 0 or less, the function prints only a
+ * newline.
+ */
+void print_triangle(int size)
+{
+int i;
+int j;
+
+for (i = 0; i < size; i++)
+{
+for(j = 0; j < size; j++)
+{
+if (size - i + 1 <= j)
+_putchar('#');
+else
+_putchar(' ');
+}
+_putchar('\n');
+}
+size <= 0 ? :_putchar('\n') : i++;
+}
