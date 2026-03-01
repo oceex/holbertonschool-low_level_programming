3c7903b5f0b437a347ce37095e246662054f3ff5
leen <leenalgraawi@gmail.com>
2026-03-01 01:22:13 +0000

    kiki

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+# include "main.h"
+# include <stdlib.h>
+/**
+ * print_number - prints an integer using _putchar
+ * @n: the integer to print
+ *
+ * Description: This function prints an integer one character at a time
+ * using only _putchar. It handles negative numbers and prints digits
+ * in the correct order using recursion.
+ */
+void print_number(int n)
+{
+if (n < 0)
+_putchar('-');
+n = abs(n);
+while (n > 0)
+{
+_putchar( n % 10 + '0');
+n/=10;
+}
+}
