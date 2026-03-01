9e3a02dd3d33f56fc9ef1acb426a1510c0256640
leen <leenalgraawi@gmail.com>
2026-03-01 00:07:45 +0000

    pripri

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+# include <stdio.h>
+/**
+ * main - prints the largest factor of the number 612852475143
+ *
+ * Description: The program iterates downward from the number itself
+ * to find the first value that divides it evenly. The first divisor
+ * encountered in this reverse loop is the largest factor, which is
+ * then printed before the program exits.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+unsigned long n = 612852475143;
+unsigned long i;
+
+for (i = n; i > 0; i--)
+{
+if (n % i == 0)
+{
+printf("%lu\n", i);
+break;
+}
+}
+return (0);
+}
