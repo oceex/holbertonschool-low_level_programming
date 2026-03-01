a5c837f3037733ce21e0e217ef5c731a0da47938
leen <leenalgraawi@gmail.com>
2026-03-01 20:25:58 +0000

    yaar

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -9,6 +9,8 @@
  */
 void print_number(int n)
 {
+int b = n;
+int k = 0;
 if (n < 0)
 {
 _putchar('-');
@@ -16,9 +18,17 @@ n = -n;
 }
 if (n == 0)
 _putchar('0');
+while (b > 0)
+{
+b /= 10;
+k++;
+}
+k--;
+b = 0;
 while (n > 0)
 {
-_putchar( n % 10 + '0');
-n/=10;
+b = n / 10**k;
+_putchar(b + '0');
+n -= b * 10**k;
 }
 }
