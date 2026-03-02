d8c996353b680e75b2d40d80f7ebf493e99d22c5
leen <leenalgraawi@gmail.com>
2026-03-02 03:09:02 +0000

    maomao

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,8 +7,11 @@
  */
 void puts2(char *str)
 {
-int i = -1;
-while ((str[++i] != '\0') && (str[i++] != '\0'))
+int i = 0;
+while ((str[i] != '\0') && (str[i + 1] != '\0'))
+{
 _putchar(str[i]);
+i += 2;
+}
 _putchar('\n');
 }
