169109cf2342562baff48c18674a51282300d752
leen <leenalgraawi@gmail.com>
2026-03-02 03:31:58 +0000

    uiui

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -11,8 +11,8 @@ void puts_half(char *str)
 int i = -1, j = 0;
 while (str[i + 1] != '\0')
 i++;
-if (i % 2 == 0)
-j = (i + 1) / 2;
+if (i % 2 != 0)
+j = (i + 2) / 2;
 else
 j = i / 2;
 for (; j <= i; j++)
