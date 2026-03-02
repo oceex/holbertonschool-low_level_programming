f237fa542a54c78d2205bfa580448c59a8d24279
leen <leenalgraawi@gmail.com>
2026-03-02 01:25:46 +0000

    strrr

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+# include "main.h"
+#include <string.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to the string to measure
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+}
