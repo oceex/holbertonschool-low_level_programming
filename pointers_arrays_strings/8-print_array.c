20bf3a34a251050a16f2b41f592dd068fce2e845
leen <leenalgraawi@gmail.com>
2026-03-02 14:43:51 +0000

    kakak

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,12 +11,10 @@
 void print_array(int *a, int n)
 {
 int i;
-int j = sizeof(*a) / sizeof(int);
+int j = sizeof(a) / sizeof(int);
 if ((n > j) || (n < 0))
-{
 printf("\n");
-return;
-}
+else
 for (i = 0; i < n; i++)
 if (i != n - 1)
 printf("%d, ", a[i]);
