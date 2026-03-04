c0562a8ff775720630d74546cc920378c34eff0e
leen <leenalgraawi@gmail.com>
2026-03-04 18:04:38 +0000

    kaka

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,17 +1,36 @@
 #include "main.h"
-
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: pointer to the string to modify
+ *
+ * Description:
+ * Converts the first letter after any separator into uppercase.
+ * Separators include space, tab, newline, period, semicolon,
+ * comma, exclamation mark, question mark, double quote,
+ * parentheses, and braces. Characters that are not lowercase
+ * alphabetic letters remain unchanged.
+ *
+ * Return: pointer to the modified string s
+ */
 char *cap_string(char *s)
 {
 int i = 0, j = 0, m = 0, n;
-char x[13] ={' ','\t','\n',',',';','.','!','?','"','(',')','{','}'};
+char x[13] ={' ','\t','.','\n',';',',','!','?','"','(',')','{','}'};
 
 while (s[i] != '\0')
-i++;
-for (; j < i; j++)
 {
+m = 0;
 for (n = 0; n < 13; n++)
-if (x[n] == s[j])
+{
+if (x[n] == s[i])
 m = 1;
-
+if ((m) && (n < 4))
+m = 2;
 }
+if ((m % 2 == 0) && (m != 0) && (s[i + 1] != '\0'))
+s[i + 1] = s[i + 1] + 32;
+
+i++;
+}
+return (s);
 }
