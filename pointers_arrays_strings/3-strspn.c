4f749d2d2a658a931758a084f05e582863345d1c
leen <leenalgraawi@gmail.com>
2026-03-08 19:37:07 +0000

    k

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -14,20 +14,19 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-int i = 0, j = 0, n = 0;
-unsigned int k = 0;
+unsigned int i = 0, j = 0;
 while (s[i] != '\0')
+{
+j = 0;
+while (accept[j] != '\0')
+{
+if (accept[j] == s[i])
+break;
+j++;
+}
+if (accept[j] == '\0')
+break;
 i++;
-i /= 2;
-for (; j < i; j++)
-{
-n = 0;
-while (accept[n] != '\0')
-{
-if (accept[n] == s[j])
-k++;
-n++;
 }
-}
-return (k);
+return (i);
 }
