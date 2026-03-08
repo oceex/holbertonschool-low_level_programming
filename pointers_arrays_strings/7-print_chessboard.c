31d5d210043c041b26c8c7a28dd0655e7309178e
leen <leenalgraawi@gmail.com>
2026-03-08 20:47:37 +0000

    ll

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_chessboard - prints an 8x8 chessboard
+ * @a: 2D array of characters representing the board
+ *
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+int i = 0, j = 0;
+for (; i < 8; i++)
+for (; j < 8; j++)
+_putchar(a[i][j]);
+}
