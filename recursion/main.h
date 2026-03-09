218d039d2004af2927ad6c25847fa7e9c9bae74a
leen <leenalgraawi@gmail.com>
2026-03-09 20:17:49 +0000

    l

--- recursion/main.h
+++ recursion/main.h
@@ -8,3 +8,6 @@ int _sqrt_recursion(int n);
 int _sqrt(int n, int i);
 int is_prime_number(int n);
 int _primi(int n, int i);
+int is_palindrome(char *s);
+int _comp(char *a, char *b);
+char *_rev_recursion(char *s);
