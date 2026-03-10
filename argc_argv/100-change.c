d90db6e123d4b11c1a0ee55e540d71c7f84b444c
leen <leenalgraawi@gmail.com>
2026-03-10 18:57:39 +0000

    k

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -3,8 +3,7 @@
 /**
  * main - Calculates the minimum number of coins for change
  * @argc: Number of arguments
- * @argv: Array of argument strings
- *
+ * @argv: Array of argument string
  * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
@@ -15,42 +14,26 @@ if (argc != 2)
 printf("Error\n");
 return (1);
 }
-else if (atoi(argv[1]) < 0)
+if (atoi(argv[1]) < 0)
 {
 printf("0\n");
+return (0);
 }
-else
-{
 n = atoi(argv[1]);
 while (n > 0)
 {
 if (n >= 25)
-{
 n -= 25;
-k++;
-}
 else if (n >= 10)
-{
 n -= 10;
-k++;
-}
 else if (n >= 5)
-{
 n -= 5;
-k++;
-}
 else if (n >= 2)
-{
 n -= 2;
-k++;
-}
 else if (n == 1)
-{
 n--;
 k++;
 }
-}
 printf("%d\n", k);
-}
 return (0);
 }
