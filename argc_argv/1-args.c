e995d8ae34424520c33a5a4b10cf0fd0500a10cb
leen <leenalgraawi@gmail.com>
2026-03-10 17:48:23 +0000

    k

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments passed to the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main (int argc)
+{
+printf("%d\n", argc);
+return (0);
+}
