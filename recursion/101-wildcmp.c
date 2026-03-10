22c9e2c0186f883d5877ce9663a272942e9f63b7
leen <leenalgraawi@gmail.com>
2026-03-10 03:01:07 +0000

    k

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -18,7 +18,7 @@ if (*(s2 + 1) == '*')
 return (wildcmp(s1, s2 + 1));
 if (*s1 == *(s2 + 1))
 return (wildcmp(s1, s2 + 1));
-if (wildcmp(s1 + 1, s2))
+if ((*s1 != '\0') && (wildcmp(s1 + 1, s2)))
 return (1);
 if (*(s2 + 1) != *s1)
 return (wildcmp(s1 + 1, s2));
