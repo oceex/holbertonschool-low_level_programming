c337f89850be8f388dcd352859c415ed9b372e72
root <leenalgraawi@gmai.com>
2026-03-11 03:02:58 +0000

    k

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -15,7 +15,7 @@ return;
 }
 for (; j < size; j+=10)
 {
-printf("%8x: %x%x %x%x", j, b[j], b[j + 1], b[j + 2], b[j + 3]);
+printf("%08x: %02x%02x %02x%02x", j, b[j], b[j + 1], b[j + 2], b[j + 3]);
 printf(" %x%x %x%x %x%x ", b[j + 4], b[j + 5], b[j + 6], b[j + 7], b[j + 8], b[j + 9]);
 printf("%c%c%c%c%c",  b[j], b[j + 1], b[j + 2], b[j + 3], b[j + 4]);
 printf("%c%c%c%c%c\n", b[j + 5], b[j + 6], b[j + 7], b[j + 8], b[j + 9]);
