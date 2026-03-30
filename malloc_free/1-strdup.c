c43309df0317e85f4f967e4f5c0bdf218cdbdc3a
root <leenalgraawi@gmai.com>
2026-03-30 14:14:21 +0000

    k

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * _strdup - it makes a double str for a spisifec string.
+  * @str: the string that we want to make a double from it.
+  *
+  * Return : returns the new string address or NULL if not.
+  */
+char *_strdup(char *str)
+{
+int i = 0, j = 0;
+char *x;
+if (str == NULL)
+return (NULL);
+while (str != '\0')
+i++;
+x = malloc(i);
+if (x == NULL)
+return (NULL);
+for (; j <= i; j++)
+x[j] = str[j];
+return (x);
+}
