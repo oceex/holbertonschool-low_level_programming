5859115c05eaf8db6c51fcb68bfd306d6fba9d25
root <leenalgraawi@gmai.com>
2026-03-30 16:43:33 +0000

    k

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,13 +12,19 @@ char *str_concat(char *s1, char *s2)
 {
 int i = 0, j = 0, z = 0, h = 0;
 char *adjoin;
+bool m = 1, n = 1;
+if ((s1 == NULL) || (s1[0] == '\0'))
+m = 0;
+if ((s2 == NULL) || (s2[0] == '\0'))
+n = 0;
 while ((s1[i] != '\0') && (s2[j] != '\0'))
 {
-if (s1[i] != '\0')
+if ((s1[i] != '\0') && (m))
 i++;
-if (s2[j] != '\0')
+if ((s2[j] != '\0') && (n))
 j++;
 }
+
 adjoin = malloc(sizeof(char) * (i + j));
 if (adjoin == NULL)
 return (NULL);
