0f167655013a712a7dd66679f70ce583129c8f41
root <leenalgraawi@gmai.com>
2026-03-30 14:52:37 +0000

    k

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * str_concat - joining two strings in new string.
+  * @s1: the first string.
+  * @s2: the second string.
+  *
+  * Return: the new string or NULL.
+  */
+char *str_concat(char *s1, char *s2)
+{
+int i = 0, j = 0, z = 0;
+char *adjoin;
+while ((s1[i] != '\0') && (s2[j] != '\0'))
+{
+if (s1[i] != '\0')
+i++;
+if (s2[j] != '\0')
+j++;
+}
+adjoin = malloc(sizeof(char) * (i + j));
+if (adjoin == NULL)
+return (NULL);
+z = i + j - 1;
+for (; z < i; z++)
+adjoin[z] = s1[z];
+for (z = 0; z <= j; z++)
+adjoin[z] = s2[z];
+return (adjoin);
+}
