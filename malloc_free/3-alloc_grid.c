333b0894887f5144281509a93c67f3bf0c1d9d62
root <leenalgraawi@gmai.com>
2026-04-01 16:37:11 +0000

    k

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -7,9 +7,15 @@ int i = 0, j = 0;
 int **k;
 if ((width <= 0) || (height <= 0))
 return (NULL);
-k = malloc(sizeof(int) * (width * height));
+k = malloc(sizeof(int *) * height));
 if (k == NULL)
 return (NULL);
+for (; j < width; j++)
+{
+k[j] = malloc(sizeof(int) * width));
+if (k[j] == NULL)
+return (NULL);
+}
 for (; i < height; i++)
 {
 for (; j < width; j++)
