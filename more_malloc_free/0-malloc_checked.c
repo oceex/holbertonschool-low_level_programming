3189ba94e7186c6557039cd531ad6915ac834a77
root <leenalgraawi@gmai.com>
2026-04-01 17:57:03 +0000

    k

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,8 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /** malloc_checked - to exit ;> the faul errors.
   * @b: the size the wanted to be allocated.
+  *
   * Return: NO return.
   */
 void *malloc_checked(unsigned int b)
