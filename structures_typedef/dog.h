5c3602848fed6549d7035a95955203f95b403d96
root <leenalgraawi@gmai.com>
2026-04-03 15:25:22 +0000

    k

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,4 +14,12 @@ char *name;
 float age;
 char *owner;
 };
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
