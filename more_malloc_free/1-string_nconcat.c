ac4c71c2db22bd9745dad04ad827b989df3fb7d0
root <leenalgraawi@gmai.com>
2026-04-03 12:52:40 +0000

    k

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+  * string_nconcat - it compain s1 and a n char of s2.
+  * @s1: the first string.
+  * @s2: the second string.
+  * @n: the sesfic amount of s2.
+  *
+  * Return: returns k or NULL.
+  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 unsigned int i = 0, j = 0, z = 0;
@@ -13,9 +20,11 @@ while (s2[j] != '\0')
 j++;
 n = (n > j)? j : n;
 k = malloc(sizeof(int) * (i + n + 1));
+if (k == NULL)
+return (NULL);
 for (; z < i; z++)
 k[z] = s1[z];
-for (; z < i + n; z++)
+for (; z < (i + n); z++)
 k[z] = s2[z - i];
 k[z] = '\0';
 return (k);
