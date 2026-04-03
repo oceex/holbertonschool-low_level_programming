c7215c0528e5c2b8b517e6a3785866db24497b9b
root <leenalgraawi@gmai.com>
2026-04-03 13:21:43 +0000

    k

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,12 +12,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 unsigned int i = 0;
 int *k;
+char *n;
 if ((nmemb == 0) || (size == 0))
 return (NULL);
 k = malloc(nmemb * size);
 if (k == NULL)
 return (NULL);
+n = (char *)k;
 for (; i < nmemb; i++)
-k[i] = 0;
-return (k);
+n[i] = 0;
+return (n);
 }
