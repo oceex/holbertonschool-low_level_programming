99eb928db6f020e324d7817a760394408ad04df4
root <leenalgraawi@gmai.com>
2026-04-03 13:44:23 +0000

    k

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -9,13 +9,13 @@
   */
 int *array_range(int min, int max)
 {
-int *k;
+int *k, i;
 if (min > max)
 return (NULL);
 k = malloc(sizeof(int) * (max - min + 1));
 if (k == NULL)
 return (NULL);
-for (; min <= max; min++)
-k[min] = min;
+for (i = 0; i < (max - min + 1); i++);
+k[i] = min;
 return (k);
 }
