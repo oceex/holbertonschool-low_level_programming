33c97d46bd8340cb97d0f931c2c2c840be9c38eb
root <leenalgraawi@gmai.com>
2026-04-03 14:24:11 +0000

    k

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,4 +1,13 @@
-struct dog{
+/**
+  * dog - this stuct contains the dog info.
+  * @name: the dog name.
+  * @age: the dog age.
+  * @owner: the dog's owner.
+  *
+  * Description: Longer description
+  */
+struct dog
+{
 char *name;
 float age;
 char *owner;
