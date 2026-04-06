55ecafbd1005a757fc36714e58ddd08b8cea6134
root <leenalgraawi@gmai.com>
2026-04-06 14:53:31 +0000

    k

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+  * array_iterator - this fun applies of all the array and do the same action.
+  * @array: the address of the array.
+  * @size: the size of the array.
+  * @action: the address of the fun.
+  *
+  * Return: NO return.
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int i = 0;
+for (; i < size; i++)
+action(array[i]);
+}
