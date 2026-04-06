31b0389029f9e0ba17caff22d591b40f44a6fc1f
root <leenalgraawi@gmai.com>
2026-04-06 15:12:23 +0000

    k

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+  * int_index - this func find tha maches num from the cmp fun.
+  * @array: the array.
+  * @size: the size of the array.
+  * @cmp: is the address of the tested func.
+  *
+  * Return: NO return value.
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i = 0;
+if (size <= 0)
+return (-1);
+for (; i < size; i++)
+if (cmp(array[i])
+return (i);
+return (-1);
+}
