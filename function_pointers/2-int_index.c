7355a25bf2996e9d4eef5f31b43ef79955c995de
root <leenalgraawi@gmai.com>
2026-04-06 15:13:38 +0000

    k

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,8 +13,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 int i = 0;
 if (size <= 0)
 return (-1);
+
 for (; i < size; i++)
-if (cmp(array[i])
+if (cmp(array[i]))
 return (i);
+
 return (-1);
 }
