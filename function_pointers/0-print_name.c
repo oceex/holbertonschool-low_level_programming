8aeee09225027050d8285a5a98602db1b8b59a41
root <leenalgraawi@gmai.com>
2026-04-06 14:41:25 +0000

    k

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+  * print_name - this fun call the reseved fun with the name
+  * @name: the name.
+  * @f: the address of a func.
+  *
+  * Return: NO return value.
+  */
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
