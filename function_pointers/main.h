e68b9b3a5b7b926ec081655b7abf751e0a420272
root <leenalgraawi@gmai.com>
2026-04-06 15:54:28 +0000

    k

---
+++ function_pointers/main.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+int main(int argc, char[] argv)
+{
+int i = 0, j = 0;
+int value = atoi(argv[i][j]);
+if (str[i][j] < '0' || argv[i][j] > '9')
+{
+printf("Error\n");
+        return (98);
+}
+pr
+return 0;
+}
