c9de9e250f808c5fd67fc58bdbc9cc9d79031c88
root <leenalgraawi@gmai.com>
2026-04-06 14:42:41 +0000

    k

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,6 @@
+#include "function_pointers.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "function_pointers.h"
 /**
   * print_name - this fun call the reseved fun with the name
   * @name: the name.
