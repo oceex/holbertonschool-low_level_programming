9df70fd9f266b0e02d640377badc3a071b98a812
root <leenalgraawi@gmai.com>
2026-04-07 06:52:49 +0000

    k

--- function_pointers/main.h
+++ function_pointers/main.h
@@ -2,15 +2,29 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
-int main(int argc, char[] argv)
+int main(int argc, char* argv[])
 {
-int i = 0, j = 0;
-int value = atoi(argv[i][j]);
-if (str[i][j] < '0' || argv[i][j] > '9')
+int (*k)(int, int);
+int i = 1, j = 0, z;
+if (argv[1][j] < '0' || argv[1][j] > '9' || argv[3][j] < '0' || argv[3][j] > '9' )
 {
 printf("Error\n");
-        return (98);
+return (98);
 }
-pr
+i = atoi(argv[1]);
+j = atoi(argv[3]);
+k = get_op_func(argv[2]);
+if (k == NULL)
+{
+printf("Error/n");
+return (99);
+}
+z = k(i, j);
+if (z == 100)
+{
+printf("Error/n");
+return (100);
+}
+printf("%d/n", z);
 return 0;
 }
