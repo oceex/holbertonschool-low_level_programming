98365688075a00c352789209bc6a2852d0698609
root <leenalgraawi@gmai.com>
2026-04-07 13:07:47 +0000

    k

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
-
-int main(int argc, char* argv[])
+/**
+  * main - this program works as a calculator receve an argumnt
+  * from the user and calculate it from the start.
+  * @argc: the number of argumant and most be 4 or you will receve an error ;>
+  * @argv: this is the array of the argumant and it
+  * most contain prognam, num1, operator, num2.
+  *
+  * Return: 98 or 99 or 100 or 0.
+  */
+int main(int argc, char *argv[])
 {
 int (*k)(int, int);
 int i = 1, j = 0, z;
-if (argc != 4 || argv[1][j] < '0' || argv[1][j] > '9' || argv[3][j] < '0' || argv[3][j] > '9')
+char m = argv[1][j], v = argv[3][j];
+if (argc != 4 || m < '0' || m > '9'|| v < '0' || v > '9')
 {
 printf("Error\n");
 return (98);
@@ -26,5 +35,5 @@ printf("Error\n");
 return (100);
 }
 printf("%d\n", z);
-return 0;
+return (0);
 }
