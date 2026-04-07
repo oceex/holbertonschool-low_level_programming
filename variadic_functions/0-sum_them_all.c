bc8aeb9b56e651ab4a7b4bc4d1781d8d30b91b05
root <leenalgraawi@gmai.com>
2026-04-07 14:22:05 +0000

    k

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,9 +11,9 @@ int sum_them_all(const unsigned int n, ...)
 {
 unsigned int i = 0;
 int m = 0;
+va_list k;
 if (n == 0)
 return (0);
-va_list k;
 va_start(k, n);
 for (; i < n; i++)
 m += va_arg (k, int);
