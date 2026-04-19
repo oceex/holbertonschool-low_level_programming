71a4a7028a18e95b5fa7e910b6ceff0ece60d8e6
root <leenalgraawi@gmai.com>
2026-04-19 15:11:29 +0000

    fixing..

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "lists.h"
+
 typedef struct list {
 char *str;
 int len;
-list_t *next;
+struct list *next;
 } list_t;
 
 size_t print_list(const list_t *h);
