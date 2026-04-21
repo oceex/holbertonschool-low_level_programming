5b8500ba9516c518d47f3816bb064e0f47f009c7
root <leenalgraawi@gmai.com>
2026-04-21 13:52:42 +0000

    freeedom

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_list - frees a list_t linked list
+ * @head: pointer to the head of the list
+ *
+ * Description:
+ * This function iterates through a singly linked list of type list_t
+ * and frees each node. It also frees the string stored in each node
+ * before freeing the node itself.
+ */
+void free_list(list_t *head)
+{
+list_t *eren, *v;
+eren = *head;
+while (eren->next != NULL)
+{
+v = *eren;
+free(eren->str);
+free(eren);
+eren = *v;
+}
+}
