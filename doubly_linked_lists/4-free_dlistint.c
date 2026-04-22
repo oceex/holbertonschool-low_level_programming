c988db39c25adf04f01f99fa5ba65c307c4a72d8
root <leenalgraawi@gmai.com>
2026-04-22 17:18:29 +0300

    freee

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -13,19 +13,12 @@
 void free_dlistint(dlistint_t *head)
 {
 dlistint_t *temp;
-if (head == NULL)
-return;
+
 while (head != NULL)
 {
 temp = head->next;
-
-if (head->next != NULL)
-free(head->next);
-
-if (head->prev != NULL)
-free(head->prev);
-
 free(head);
 head = temp;
 }
+
 }
