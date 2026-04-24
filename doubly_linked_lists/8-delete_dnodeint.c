09e81b891d32c9a40df2fccd798e006232870efb
root <leenalgraawi@gmai.com>
2026-04-24 19:47:36 +0300

    del

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -9,44 +9,44 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *tmp;
-    unsigned int i = 0;
+dlistint_t *tmp;
+unsigned int i = 0;
 
-    if (head == NULL || *head == NULL)
-        return (-1);
+if (head == NULL || *head == NULL)
+return (-1);
 
-    tmp = *head;
+tmp = *head;
 
-    if (index == 0)
-    {
-        *head = tmp->next;
-        if (tmp->next != NULL)
-            tmp->next->prev = NULL;
+if (index == 0)
+{
+*head = tmp->next;
+if (tmp->next != NULL)
+tmp->next->prev = NULL;
 
-        free(tmp);
-        return (1);
-    }
+free(tmp);
+return (1);
+}
 
 
-    while (tmp != NULL && i < index)
-    {
-        tmp = tmp->next;
-        i++;
-    }
+while (tmp != NULL && i < index)
+{
+tmp = tmp->next;
+i++;
+}
 
-    if (tmp == NULL)
-        return (-1);
+if (tmp == NULL)
+return (-1);
 
-    if (tmp->next == NULL)
-    {
-        tmp->prev->next = NULL;
-        free(tmp);
-        return (1);
-    }
+if (tmp->next == NULL)
+{
+tmp->prev->next = NULL;
+free(tmp);
+return (1);
+}
 
-    tmp->prev->next = tmp->next;
-    tmp->next->prev = tmp->prev;
+tmp->prev->next = tmp->next;
+tmp->next->prev = tmp->prev;
 
-    free(tmp);
-    return (1);
+free(tmp);
+return (1);
 }
