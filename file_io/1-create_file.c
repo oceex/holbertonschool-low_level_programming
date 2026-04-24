86f8406387cb402fbd638d5fa2b64a1a0e6d6634
root <leenalgraawi@gmai.com>
2026-04-25 00:13:51 +0300

    creating

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd, i = 0;
+int fd;
 ssize_t w;
 
 if (filename == NULL)
--- file_io/main.h
+++ file_io/main.h
@@ -6,7 +6,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <limits.h>
-
+#include <string.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 
