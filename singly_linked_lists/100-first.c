f7321f47cb7feaf52ed7e9c11dc01882691db263
root <leenalgraawi@gmai.com>
2026-04-25 02:58:29 +0300

    beforr

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,10 +1,13 @@
-#include "100-main.c"
+#include <stdio.h>
+
+void before_main(void) __attribute__((constructor));
 
 /**
- * before_main - runs before main
+ * before_main - prints a message before main is executed
  */
-void before_main(void)  __attribute__((constructor))
+void before_main(void)
 {
     printf("You're beat! and yet, you must allow,\n");
     printf("I bore my house upon my back!\n");
 }
+
