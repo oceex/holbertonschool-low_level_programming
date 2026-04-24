dcb55845953febde7bb69161cc57eae97425f804
root <leenalgraawi@gmai.com>
2026-04-24 20:43:07 +0300

    now we are reading

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to STDOUT
+ * @filename: Pointer to the name of the file to read
+ * @letters: Number of bytes to read and print
+ *
+ * Return: The actual number of bytes read and printed,
+ *         or 0 on failure or if filename is NULL
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int file;
+ssize_t reading;
+char buff[letters];
+file = open(filename,O_RDONLY);
+if (file == -1)
+return (0);
+reading = read(file, buff, letters);
+if (reading == -1)
+return (0);
+
+return (reading);
+}
