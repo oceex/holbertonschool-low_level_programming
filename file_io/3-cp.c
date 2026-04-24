4ed1d678c5eb5302396248203c9d30c5f964261f
root <leenalgraawi@gmai.com>
2026-04-25 02:10:32 +0300

    copying

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -49,7 +49,7 @@ return (97);
 f1= open(argv[1], O_RDONLY);
 if (f1 == -1)
 {
-dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+dprintf(1, "Error: Can't read from file %s\n", argv[1]);
 return (98);
 }
 f2 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
