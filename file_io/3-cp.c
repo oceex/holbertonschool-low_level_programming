f5e1fe00a928c951b7dc5385add60d50381bef6b
root <leenalgraawi@gmai.com>
2026-04-25 01:46:29 +0300

    copying

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -58,10 +58,10 @@ if (f2 == -1)
 dprintf(2, "Error: Can't write to %s\n", argv[2]);
 return (99);
 }
-r = read(f1, buf, 1024);
+r = read(f1, buf, 1023);
 if (r == -1)
 {
-dprintf(2, "Erro1r: Can't read from file %s\n", argv[1]);
+dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 return (98);
 }
 while (r > 0)
