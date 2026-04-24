add6d371ccc5bc20cc37c7938e0ebab72d9f60af
root <leenalgraawi@gmai.com>
2026-04-25 00:35:19 +0300

    appending

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * append_text_to_file - Appends text at the end of a file
+ * @filename: Name of the file to append to
+ * @text_content: NULL-terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+int fd;
+ssize_t w;
+if (filename == NULL)
+return (-1);
+else if (text_content == NULL)
+return (1);
+
+fd = open(filename, O_APPEND | O_WRONLY);
+if (fd == -1)
+return (-1);
+w = write(filename, text_content, strlen(text_content));
+if (w == -1)
+return (-1);
+return (1);
+}
