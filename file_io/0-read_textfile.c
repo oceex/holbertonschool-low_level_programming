349c0909ff92da4a122fd5717bbab3f4f38f4257
root <leenalgraawi@gmai.com>
2026-04-24 20:52:48 +0300

    reading

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -11,7 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 int file;
 ssize_t reading;
-char buff[letters];
+char buff[1024];
 file = open(filename,O_RDONLY);
 if (file == -1)
 return (0);
