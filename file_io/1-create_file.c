1856d08aec91296874de1849e01aab3e4aa85fed
root <leenalgraawi@gmai.com>
2026-04-25 00:12:13 +0300

    creating

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * create_file - Creates a file with given text content
+ * @filename: Name of the file to create
+ * @text_content: NULL-terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
 int create_file(const char *filename, char *text_content)
 {
 int fd, i = 0;
@@ -11,17 +17,15 @@ return (-1);
 fd = open(filename, O_TRUNC | O_CREAT, 0600);
 if (fd == -1)
 return (-1);
-while (text_content[i] != '\0')
-i++;
-if (text_content == NULL)
-i = 0;
-else
-w = write (fd, text_content, sizeof(char)*i);
+if (strlen(text_content) != 0)
+w = write (fd, text_content, strlen(text_content));
+
 if (w == -1)
 {
 close(fd);
 return (-1);
 }
+
 close(fd);
 return (1);
 }
