bee7a24ab53a69552256d3f7681f6fccfbea25be
root <leenalgraawi@gmai.com>
2026-04-27 23:12:35 +0300

    printing

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - Print all key/value pairs in a hash table
+ * @ht: Pointer to the hash table to print
+ *
+ * Description:
+ * Iterates through all buckets of the hash table and prints each key/value
+ * pair in the format {'key': 'value', ...}. Keys are printed in the order
+ * they appear in each linked list. The function does not modify the table.
+ * If the hash table is NULL, nothing is printed.
+ *
+ * Return:
+ * Nothing.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+unsigned long int i = 0;
+hash_node_t *tmp;
+int u = 0;
+printf("{");
+for (; i < ht->size; i++)
+{
+if (ht->array[i] == NULL)
+continue;
+tmp = malloc(sizeof(hash_node_t));
+if (tmp == NULL)
+return;
+tmp = ht->array[i];
+while (tmp)
+{
+if (v)
+printf(", ");
+
+prnitf("%s: %s", tmp->key, tmp->value);
+
+v = 1;
+}
+}
+
+}
