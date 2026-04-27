11144cb3396f8d401d2432773afdc9ea7c16b3b9
root <leenalgraawi@gmai.com>
2026-04-27 16:26:05 +0300

    the last error i guess!

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 char *val, *ke;
 unsigned long int index;
-hash_node_t *k;
+hash_node_t *k = NULL;
 if (key == NULL)
 return (0);
 
@@ -44,6 +44,7 @@ if (ht->array[index] != NULL)
 k = add_node(ht->array[index], key, val);
 if (k == NULL)
 return (0);
+free(k);
 }
 else
 {
