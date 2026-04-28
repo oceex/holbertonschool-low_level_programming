21379075fed17a0ed6f2e520f108ee2adebea758
root <leenalgraawi@gmai.com>
2026-04-28 15:32:09 +0300

    freedom

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,37 @@
+#include "hash_table.h"
+
+/**
+ * hash_table_delete - Free an entire hash table and all its nodes
+ * @ht: Pointer to the hash table to be deleted
+ *
+ * Description:
+ * Iterates through all indices of the hash table array. For each index,
+ * the function traverses the linked list stored there, freeing every node
+ * along with its allocated key and value strings. After all chains are
+ * cleared, the function frees the array itself and finally frees the
+ * hash table structure. The function performs no action if @ht is NULL.
+ *
+ * Return:
+ * Nothing. This function does not return a value.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+unsigned long int i = 0;
+hash_node_t *tmp;
+for(; i < ht->size; i++)
+{
+if (ht->array[i] == NULL)
+continue;
+
+while (ht->array[i])
+{
+tmp = ht->array[i]->next;
+free(ht->array[i]->key);
+free(ht->array[i]->value);
+free(ht->array[i]);
+ht->array[i] = tmp;
+}
+}
+free(ht->array);
+free(ht);
+}
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -44,5 +44,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 void free_two(void *p1, void *p2);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 
 #endif
